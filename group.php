b22deea459ebb680aa3c5c6c41fb3530097bdac6
jim winstead <jimw@php.net>
2001-03-11 00:46:22 +0000

    make article listing full width, right-align article number

--- group.php
+++ group.php
@@ -14,7 +14,7 @@ $n = min($l,$i+20);
 $res = nntp_cmd($s,"XOVER $i-$n", 224)
   or die("failed to get xover data\n");
 head($group);
-echo "<table class=\"alist\">";
+echo '<table class="alist" width="100%">';
 echo '<tr><td class="alisthead">#</td><td class="alisthead">subject</td><td class="alisthead">author</td><td class="alisthead">date</td><td class="alisthead">lines</td></tr>',"\n";
 $class = "even";
 while ($line = fgets($s, 4096)) {
@@ -24,7 +24,7 @@ while ($line = fgets($s, 4096)) {
     = explode("\t", $line, 9);
   $date = date("H:i:s M/d/y", strtotime($date));
   echo "<tr>";
-  echo "<td class=\"$class\">$n</td>";
+  echo "<td align=\"right\" class=\"$class\">$n</td>";
   echo "<td class=\"$class\">";
   echo "<a href=\"article.php?group=$group&amp;article=$n\">".format_subject($subj)."</a>";
   echo "</td>";
