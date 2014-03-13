290e707093c134324a215b73e91a35c9e4078d6f
Sobak <msobaczewski@gmail.com>
2014-03-13 07:29:58 +0100

    Get rid of awful spacer hack

--- common.php
+++ common.php
@@ -45,18 +45,13 @@ function head($title="PHP news") {
   <link rel="stylesheet" href="/style.css" type="text/css" />
  </head>
  <body>
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-   <tr class="header">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" class="header">
+   <tr>
     <td>
      <a href="/index.php"><img src="/i/l.gif" width="120" height="67" alt="PHP" /></a>
     </td>
     <td align="right" valign="bottom">
      PHP.net <a href="news://<?php echo $_SERVER['HTTP_HOST']; ?>/" class="top">news server</a> web interface
-    </td>
-   </tr>
-   <tr class="subheader">
-    <td colspan="2">
-     <img src="/i/g.gif" width="1" height="1" alt="" />
     </td>
    </tr>
   </table>
